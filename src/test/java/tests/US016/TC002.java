c6b0b9eca633f452ab4a28846297675115e3e1ad
MansoorAbbasi <mansoorfaizy9541@gmail.com>
2022-10-22 10:41:37 +0300

    mansoor

--- src/test/java/tests/US016/TC002.java
+++ src/test/java/tests/US016/TC002.java
@@ -2,6 +2,7 @@ package tests.US016;
 
 import org.testng.annotations.Test;
 import pages.AlloverPage;
+import utilities.ReusableMethods;
 
 public class TC002 {
     AlloverPage alloverPage = new AlloverPage();
@@ -13,8 +14,11 @@ public class TC002 {
         //4."Username or email adress" bölümüne vendor email girilir
         //5."Password" bölümüne verdor password girilir
         //6."Sing in" butonuna tıklanır.
+        ReusableMethods.signIn();
         //7.My Account’a tıklanır.
+        alloverPage.jse.executeScript("arguments[0].click();",alloverPage.myAccount);
         //8.Store manager’a tıklanır ve sonrasında Customer’a tıklanır
+        alloverPage.storeManagerButton.click();
         //9. Pdf Excel ve Csv olarak indirilebilirliği test edilir.
     }
 }
